a6b815bcf316ac6c45c85bd063e942fd73814ee6
madhavtaneja131-cmyk <tanejamadhav155@gmail.com>
2026-01-29 11:48:51 +0530

    task 1

---
+++ task 1/script.js
@@ -0,0 +1,12 @@
+const profile=document.getElementById("profile");
+profile.style.backgroundColor="tomato";
+profile.style.textAlign="center";
+profile.style.padding="15px";
+
+// //  Task 2
+// const highlight = document.getElementsByClassName("important");
+// console.log(highlight)
+// for(let i=0; i<highlight.length; i++){
+//     highlight[i].style.color="red"
+    
+// }
