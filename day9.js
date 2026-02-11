4194ab02a01e86147434acd4b42db7f723e9e089
madhavtaneja131-cmyk <tanejamadhav155@gmail.com>
2026-02-11 19:55:31 +0530

    Assignment 1

--- day9.js
+++ day9.js
@@ -1,5 +1,5 @@
 document.getElementById("myInput").addEventListener("change",(event)=>{
     // here event is the syntheitic event which comes under addeventlistener's callback function. It will print the event object which stores all information about the form tag.
     console.log(event.target.value);
-    document.getElementById("output").ATTRIBUTE_NODE.textContent=change detected;
+    document.getElementById("output").textContent=change detected;
 })
