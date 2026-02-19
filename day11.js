c629347e270562cd8a9267602dc82f3a6309237f
madhavtaneja131-cmyk <tanejamadhav155@gmail.com>
2026-02-19 11:27:02 +0530

    Day 12

--- day11.js
+++ day11.js
@@ -1,27 +1,26 @@
-// function login(email, password, callback) {
-//     setTimeout(() => {
-//         callback({ userid: email,isloggedin: true , message: "Login successful"});
-//     }, 3000);
-// }
-// login("user@example.com", "password123", (userdetails) => {
-//     console.log(userdetails);
-// });
-
-function getVideolist(cb){
-    setTimeout(()=>{
-        cb({title:"video-1", duration:"2 min"});
-    },1000);
-
-}
-
 function login(email, password, callback) {
     setTimeout(() => {
         callback({ userid: email,isloggedin: true , message: "Login successful"});
     }, 3000);
 }
+
+function getVideolist(user,cb){
+    setTimeout(()=>{
+        cb(["video-1", "video 2","video 3"])
+    },2000);
+
+}
+
+function getVideolist(video,callback) {
+    setTimeout(() => {
+        callback({ title:video,duration:"2 mins"});
+    }, 1000);
+}
+
 login("user@example.com", "password123", (userdetails) => {
-    console.log(userdetails);
-    getVideolist((videoList)=>{
-        console.log(videoList);
+    console.log(userdetails);    
+    getVideolist(userdetails.userid,(videoList)=>{
+        console.log(videoList)
+        
     })
 });
